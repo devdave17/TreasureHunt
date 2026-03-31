cb0fe317533df9deabbeae60096c25cad4040a34
Dev Dave <davedev7867@gmail.com>
2026-03-31 15:06:22 +0530

    Fixed wildcard route

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/admin", adminAuth, adminRoutes)
 // 🔥 IMPORTANT: FRONTEND SERVE (YEH ADD KARNA HAI)
 app.use(express.static(path.join(__dirname, "dist")))
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
